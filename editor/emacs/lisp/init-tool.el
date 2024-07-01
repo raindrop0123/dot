feeef9bb1748eb05775123d70151dcb48447ab6b
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-07-01 21:07:12 +0800

    Add files via upload

---
+++ editor/emacs/lisp/init-tool.el
@@ -0,0 +1,43 @@
+;;; init-tool.el --- -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require-package 'rainbow-delimiters)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(require-package 'rainbow-mode)
+(add-hook 'prog-mode-hook #'rainbow-mode)
+
+(require-package 'symbol-overlay)
+(add-hook 'prog-mode-hook #'symbol-overlay-mode)
+
+(require-package 'avy)
+(global-set-key (kbd "M-g w") 'avy-goto-word-0)
+(global-set-key (kbd "M-g l") 'avy-goto-line)
+
+(require-package 'which-key) ;; it may be a builtin tool in version 30
+(add-hook 'after-init-hook #'which-key-mode)
+(with-eval-after-load 'which-key
+  (setq which-key-idle-delay 0.01))
+
+(require-package 'diredfl)
+(add-hook 'dired-mode-hook #'diredfl-mode)
+
+(require-package 'xclip)
+(add-hook 'after-init-hook #'xclip-mode)
+
+(require-package 'winum)
+(add-hook 'after-init-hook #'winum-mode)
+
+(require-package 'ace-window)
+(global-set-key [remap other-window] 'ace-window)
+(with-eval-after-load 'ace-window
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
+
+(require-package 'gcmh)
+(add-hook 'after-init-hook #'gcmh-mode)
+
+(require-package 'esup)
+
+(provide 'init-tool)
+;;; init-tool.el ends here
