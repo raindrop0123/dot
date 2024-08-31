94ed83d722327fafeccd7751f6f0e6a4c57a9301
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-08-31 17:54:40 +0800

    Update rc.lua

--- linux/config/awesome/rc.lua
+++ linux/config/awesome/rc.lua
@@ -142,22 +142,22 @@ awful.layout.layouts = {
 -- SUBMENU: awesome
 awesomemenu = {
   { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-  { "manual", "urxvt -e man awesome" },
-  { "edit config", "urxvt -e vim " .. awesome.conffile },
+  { "manual", "alacritty -e man awesome" },
+  { "edit config", "alacritty -e vim " .. awesome.conffile },
   { "restart", awesome.restart },
   { "quit", function() awesome.quit() end },
 }
 -- SUBMENU: editor
 editormenu = {
   { "Emacs", "emacs" },
-  { "vim", "urxvt -e vim" },
-  { "neovim", "urxvt -e nvim" },
+  { "vim", "alacritty -e vim" },
+  { "neovim", "alacritty -e nvim" },
 }
 -- MAIN MENU
 mainmenu = awful.menu({
   items = {
     { " AwesomeWM ", awesomemenu, beautiful.awesome_icon },
-    { " Terminal ", "urxvt" },
+    { " Terminal ", "alacritty" },
     { " Editor ", editormenu },
   }
 })
@@ -167,7 +167,7 @@ launcher = awful.widget.launcher({
   menu = mainmenu
 })
 -- Menubar configuration
-menubar.utils.terminal = "urxvt"
+menubar.utils.terminal = "alacritty"
 -- }}}
 
 -- {{{ WIBAR
@@ -253,7 +253,7 @@ end)
 
 -- {{{ KEY BINDING & MOUSE BINDING
 globalkeys = gears.table.join(
-  awful.key({ modkey }, "Return", function() awful.spawn("urxvt") end, { description = "open a terminal", group = "launcher" }),
+  awful.key({ modkey }, "Return", function() awful.spawn("alacritty") end, { description = "open a terminal", group = "launcher" }),
   awful.key({ modkey }, "j", function() awful.client.focus.byidx(1) end, { description = "focus next by index", group = "client" }),
   awful.key({ modkey }, "k", function() awful.client.focus.byidx(-1) end, { description = "focus previous by index", group = "client" }),
   awful.key({ modkey, "Shift" }, "j", function() awful.client.swap.byidx(1) end, { description = "swap with next client by index", group = "client" }),
