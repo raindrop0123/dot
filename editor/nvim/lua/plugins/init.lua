0266eb5b6554a0d13211d9edb5c2e32ca856d381
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-09-07 23:08:47 +0800

    Add files via upload

---
+++ editor/nvim/lua/plugins/init.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    lazy = true,
+    event = "InsertEnter",
+    opts = {},
+  },
+  {
+    "numToStr/Comment.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    opts = {},
+  },
+}
