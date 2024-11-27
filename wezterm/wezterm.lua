31646b9c9741181890dbe99a63f6e9cd0810836a
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-11-27 13:42:58 +0800

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,108 +5,111 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- [[ FONT ]]
-config.font_size = 9.0
+---@FONT
+config.font_size = 8.0
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 
--- [[ THEME ]]
+---@THEME
 --config.color_scheme = "Default Dark (base16)"
 config.colors = {
-  foreground = "#cdd6f4",
-  background = "#11111b",
-  cursor_fg = "#11111b",
-  cursor_bg = "#f5e0dc",
-  cursor_border = "#f5e0dc",
-  selection_fg = "#11111b",
-  selection_bg = "#89dceb",
-  scrollbar_thumb = "#6c7086",
-  split = "#9399b2",
+  foreground = "#d8d8d8",
+  background = "#181818",
+  cursor_fg = "#181818",
+  cursor_bg = "#f7ca88",
+  cursor_border = "#f7ca88",
+  selection_fg = "#f8f8f8",
+  selection_bg = "#585858",
+  scrollbar_thumb = "#585858",
+  split = "#585858",
   ansi = {
-    "#1e1e2e",
-    "#f38ba8",
-    "#a6e3a1",
-    "#f9e2af",
-    "#89b4fa",
-    "#cba6f7",
-    "#74c7ec",
-    "#cdd6f4",
+    "#181818",
+    "#ab4642",
+    "#a1b56c",
+    "#f7ca88",
+    "#7cafc2",
+    "#ba8baf",
+    "#86c1b9",
+    "#d8d8d8",
   },
   brights = {
-    "#1e1e2e",
-    "#f38ba8",
-    "#a6e3a1",
-    "#f9e2af",
-    "#89b4fa",
-    "#cba6f7",
-    "#74c7ec",
-    "#cdd6f4",
+    "#585858",
+    "#ab4642",
+    "#a1b56c",
+    "#f7ca88",
+    "#7cafc2",
+    "#ba8baf",
+    "#86c1b9",
+    "#f8f8f8",
   },
-  compose_cursor = "#f2cdcd",
+  compose_cursor = "#f7ca88",
   tab_bar = {
-    background = "#11111b",
-    inactive_tab_edge = "#11111b",
+    background = "#181818",
+    inactive_tab_edge = "#181818",
     active_tab = {
-      bg_color = "#11111b",
-      fg_color = "#a6e3a1",
+      bg_color = "#181818",
+      fg_color = "#f7ca88",
       intensity = "Bold",
       underline = "None",
       italic = true,
       strikethrough = true,
     },
     inactive_tab = {
-      bg_color = "#11111b",
-      fg_color = "#b4befe",
+      bg_color = "#181818",
+      fg_color = "#d8d8d8",
       intensity = "Normal",
       underline = "None",
       italic = true,
       strikethrough = true,
     },
     inactive_tab_hover = {
-      bg_color = "#11111b",
-      fg_color = "#a6e3a1",
+      bg_color = "#181818",
+      fg_color = "#d8d8d8",
       intensity = "Normal",
       underline = "None",
       italic = true,
       strikethrough = true,
     },
     new_tab = {
-      bg_color = "#11111b",
-      fg_color = "#cdd6f4",
+      bg_color = "#181818",
+      fg_color = "#d8d8d8",
       italic = true,
     },
     new_tab_hover = {
-      bg_color = "#11111b",
-      fg_color = "#cdd6f4",
+      bg_color = "#181818",
+      fg_color = "#d8d8d8",
       italic = true,
     },
   },
 }
 config.window_frame = {
-  font = wezterm.font { family = "JetBrainsMono Nerd Font", weight = "Bold" },
+  font = wezterm.font {
+    family = "JetBrainsMono Nerd Font",
+    weight = "Bold"
+  },
   font_size = 8.0,
-  active_titlebar_bg = "#11111b",
-  inactive_titlebar_bg = "#11111b",
+  active_titlebar_bg = "#181818",
+  inactive_titlebar_bg = "#181818",
 }
 
--- [[ UI ]]
-config.line_height = 1.0
+---@UI
+config.line_height = 1.2
 config.default_cursor_style = "BlinkingBlock"
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 config.window_padding = {
-  left = 8,
-  right = 8,
-  top = 8,
-  bottom = 8,
+  left = 12,
+  right = 12,
+  top = 12,
+  bottom = 12,
 }
 
--- [[ ANIMATION ]]
+---@ANIMATION
 config.animation_fps = 1
 
--- [[ TAB ]]
-config.hide_tab_bar_if_only_one_tab = true
+---@TAB
+config.hide_tab_bar_if_only_one_tab = false
 
--- [[ CUSTOM ]]
+---@CUSTOM
 config.window_close_confirmation = "NeverPrompt"
 
 return config
