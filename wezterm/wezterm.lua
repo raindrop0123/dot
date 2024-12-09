57bbc659934a20c826e98cdae6ef6c455dade6c2
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2024-12-09 09:03:42 +0800

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,28 +2,38 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
-config.font_size = 11.25
-config.font = wezterm.font("JetBrainsMono Nerd Font")
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.font_size = 9.00
+  config.font = wezterm.font("JetBrainsMono Nerd Font")
+  config.default_prog = { "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
+  config.window_background_opacity = 0.90
+elseif wezterm.target_triple == "aarch64-apple-darwin" then
+  config.font_size = 11.25
+  config.font = wezterm.font("JetBrains Mono")
+  config.default_prog = { "zsh", "-l" }
+  config.window_background_opacity = 0.90
+elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+  config.font_size = 10.00
+  config.font = wezterm.font("JetBrainsMono Nerd Font")
+  config.default_prog = { "bash", "-l" }
+  config.window_background_opacity = 1.00
+end
 config.color_scheme = "Default Dark (base16)"
-config.window_background_opacity = 0.95
 config.line_height = 1.0
 config.default_cursor_style = "BlinkingBlock"
 config.animation_fps = 1
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 config.window_padding = {
-	left = 5,
-	right = 5,
-	top = 5,
-	bottom = 5,
+  left = 5,
+  right = 5,
+  top = 5,
+  bottom = 5,
 }
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = "NeverPrompt"
-config.command_palette_font_size = 11.25
-config.command_palette_bg_color = "#181818"
-config.command_palette_fg_color = "#d8d8d8"
 
 return config
