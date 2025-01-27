ff5c9700d24bf81a042af1e8e1560fcbed21dd40
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2025-01-27 21:58:28 +0800

    Update init.el

--- cross/emacs/init.el
+++ cross/emacs/init.el
@@ -35,13 +35,13 @@
   (setq package-check-signature nil)
   (setq package-quickstart t)
   (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                          ;; ("elpa-devel" . "https://elpa.gnu.org/devel/")
-													 ;; ("org" . "https://orgmode.org/elpa/")
-													 ;; ("marmalade" . "http://marmalade-repo.org/packages/")
-													 ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
-													 ;; ("jcs-elpa" . "https://jcs-emacs.github.io/jcs-elpa/packages/")
-                          ("gnu" . "https://elpa.gnu.org/packages/")
-													 ("nongnu" . "https://elpa.nongnu.org/nongnu/"))))
+                           ;; ("elpa-devel" . "https://elpa.gnu.org/devel/")
+			   ;; ("org" . "https://orgmode.org/elpa/")
+			   ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+			   ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
+			   ;; ("jcs-elpa" . "https://jcs-emacs.github.io/jcs-elpa/packages/")
+                           ("gnu" . "https://elpa.gnu.org/packages/")
+			   ("nongnu" . "https://elpa.nongnu.org/nongnu/"))))
 
 (use-package emacs
   :ensure nil
