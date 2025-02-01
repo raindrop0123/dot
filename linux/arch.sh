10d19520d2a7ebd434e2a26e1e8911f733b6eaa0
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2025-02-01 22:25:17 +0800

    Update arch.sh

--- linux/arch.sh
+++ linux/arch.sh
@@ -189,7 +189,7 @@ RUST="lsd ripgrep bottom fzf fd bat rust"
 JS="npm"
 TERM="alacritty kitty wezterm xterm rxvt-unicode ghostty"
 EDITOR="emacs neovim gvim helix nano kakonue"
-GUITOOL="pcmanfm lxappearance qt5ct firefox"
+GUITOOL="pcmanfm lxappearance qt5ct firefox waybar"
 arch-chroot /mnt sudo pacman -S --noconfirm --needed $WM $HYPR $SYSTOOL1 $SYSTOOL2 $SYSTOOL3
 arch-chroot /mnt sudo pacman -S --noconfirm --needed $INPUT $RUST $TERM $EDITOR $GUITOOL $JS
 ADOBE1="adobe-source-code-pro-fonts	adobe-source-han-sans-cn-fonts adobe-source-han-sans-hk-fonts"
