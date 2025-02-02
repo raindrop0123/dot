c49867c053b1ac24567ecea2a97ebbdbdbce4315
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2025-02-02 13:35:34 +0800

    Update wezterm.lua

--- cross/wezterm/wezterm.lua
+++ cross/wezterm/wezterm.lua
@@ -21,7 +21,7 @@ elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
   config.default_prog = { "bash", "-l" }
   config.window_background_opacity = 0.85
 end
-config.color_scheme = "Default Dark (base16)"
+config.color_scheme = "Catppuccin Mocha"
 config.line_height = 1.00
 config.default_cursor_style = "BlinkingBlock"
 config.animation_fps = 1
